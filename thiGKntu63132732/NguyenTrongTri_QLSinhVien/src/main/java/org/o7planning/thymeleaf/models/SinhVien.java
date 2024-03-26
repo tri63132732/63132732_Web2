962de15d7e1e09755febc257ac1f75f8a0e5bf2c
tri63132732 <144207384+tri63132732@users.noreply.github.com>
2024-03-26 15:31:52 +0700

    update

--- thiGKntu63132732/NguyenTrongTri_QLSinhVien/src/main/java/org/o7planning/thymeleaf/models/SinhVien.java
+++ thiGKntu63132732/NguyenTrongTri_QLSinhVien/src/main/java/org/o7planning/thymeleaf/models/SinhVien.java
@@ -3,6 +3,8 @@ package org.o7planning.thymeleaf.models;
 public class SinhVien {
 	private String maSoSV;
 	private String hoVaTen;
+	private String khoa;
+	private String truong;
 	public String getMaSoSV() {
 		return maSoSV;
 	}
@@ -15,8 +17,22 @@ public class SinhVien {
 	public void setHoVaTen(String hoVaTen) {
 		this.hoVaTen = hoVaTen;
 	}
-	public SinhVien(String maSoSV, String hoVaTen) {
+	public String getKhoa() {
+		return khoa;
+	}
+	public void seKhoa(String khoa) {
+		this.khoa = khoa;
+	}
+	public String getTruong() {
+		return truong;
+	}
+	public void setTruong(String truong) {
+		this.truong = truong;
+	}
+	public SinhVien(String maSoSV, String hoVaTen, String khoa, String truong) {
 		this.maSoSV = maSoSV;
 		this.hoVaTen = hoVaTen;
+		this.khoa = khoa;
+		this.truong = truong;
 	}
 }
