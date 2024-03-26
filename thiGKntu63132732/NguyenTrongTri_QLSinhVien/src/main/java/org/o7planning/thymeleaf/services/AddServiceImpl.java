e62fd39435c52521c2e23de4f0ecdc80272bf06f
tri63132732 <144207384+tri63132732@users.noreply.github.com>
2024-03-26 16:35:25 +0700

    update

--- thiGKntu63132732/NguyenTrongTri_QLSinhVien/src/main/java/org/o7planning/thymeleaf/services/AddServiceImpl.java
+++ thiGKntu63132732/NguyenTrongTri_QLSinhVien/src/main/java/org/o7planning/thymeleaf/services/AddServiceImpl.java
@@ -1,5 +1,20 @@
 package org.o7planning.thymeleaf.services;
 
-public class AddServiceImpl {
+import org.springframework.stereotype.Service;
 
+import org.o7planning.thymeleaf.models.SinhVien;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+@Service
+public class AddServiceImpl implements AddService {
+
+    
 }
