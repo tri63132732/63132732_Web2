f01fe9d9c7011557d3d96271990c94a67e836343
tri63132732 <144207384+tri63132732@users.noreply.github.com>
2024-04-16 15:47:58 +0700

    update

--- ThiCuoiKi63132732/src/main/java/org2/NuyenTrongTri/controller/MainController.java
+++ ThiCuoiKi63132732/src/main/java/org2/NuyenTrongTri/controller/MainController.java
@@ -1,15 +1,15 @@
 package org2.NuyenTrongTri.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
-	@GetMapping("/")
-	public String index(Model model) {
-
-		return "index";
-	}
-
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
 }
 
 
