1393ccf2c041d8ecc5bc421edcf2b59eff8b8f2d
tri63132732 <144207384+tri63132732@users.noreply.github.com>
2024-04-16 15:32:32 +0700

    update

---
+++ ThiCuoiKi63132732/src/main/java/org2/NuyenTrongTri/ThiCuoiKi63132732Application.java
@@ -0,0 +1,13 @@
+package org2.NuyenTrongTri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiCuoiKi63132732Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiCuoiKi63132732Application.class, args);
+	}
+
+}
---
+++ ThiCuoiKi63132732/src/test/java/org2/NuyenTrongTri/ThiCuoiKi63132732ApplicationTests.java
@@ -0,0 +1,13 @@
+package org2.NuyenTrongTri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiCuoiKi63132732ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
