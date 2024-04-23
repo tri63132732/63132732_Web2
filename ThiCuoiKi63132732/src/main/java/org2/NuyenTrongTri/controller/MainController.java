c84e46077e125201ee54ddd8e61a48f45dd74dab
tri63132732 <144207384+tri63132732@users.noreply.github.com>
2024-04-23 15:02:53 +0700

    update

--- ThiCuoiKi63132732/src/main/java/org2/NuyenTrongTri/controller/MainController.java
+++ ThiCuoiKi63132732/src/main/java/org2/NuyenTrongTri/controller/MainController.java
@@ -1,16 +1,26 @@
 package org2.NuyenTrongTri.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
-    public String index(Model model) {
-        // Thêm dữ liệu "message" vào Model để truyền đến view
-        model.addAttribute("message", "hello");
-        // Trả về tên của view hoặc đường dẫn đến view cần hiển thị
-        return "index";
-    }
+	@GetMapping("/")
+	public String index() {
+		return "index.html";
+	}
+	@GetMapping("/about")
+	public String about() {
+		return "about.html";
+	}
+	@GetMapping("/service")
+	public String sevrice() {
+		return "service.html";
+	}
+	
+	
+	@GetMapping("/mau")
+	public String mau() {
+		return "page-mau-HTML.html";
+	}
 }
